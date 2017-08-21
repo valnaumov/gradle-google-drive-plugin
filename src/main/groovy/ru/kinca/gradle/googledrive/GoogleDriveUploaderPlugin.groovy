aabb73f4860128a73de63517d7a26d9fedb52ede
valnaumov <cheshiretomcat@mail.ru>
2017-08-21 15:47:23 +0800

    Gradle setup for plugin development.

---
+++ src/main/groovy/ru/kinca/gradle/googledrive/GoogleDriveUploaderPlugin.groovy
@@ -0,0 +1,18 @@
+package ru.kinca.gradle.googledrive
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * A plugin that compresses specified files to an archive and uploads it to
+ * Google Drive.
+ *
+ * @author Valentin Naumov.
+ */
+class GoogleDriveUploaderPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        println 'GoogleDriveUploaderPlugin applied.'
+    }
+}
